97ccee3096e8ba8753f0c229cd3b687e4b3e07fe
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-07-18 13:31:33 -0700

    added html, css and js. event listener for js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// item should show when button is click
+// create variable for display items
+
+const inputItems = document.getElementById("inputField");
+const btn = document.getElementById("btn");
+const shoppingListItems = document.getElementById("shoppingList");
+
+btn.addEventListener("click", addToCart);
+
+function addToCart() {
+  alert("received");
+}
+
+function displayShoppingItems() {}
